b48b84486dcf7b91f295afcadd6b70679d3c67a8
kalastus33 <kalastus33@yahoo.co.jp>
2019-10-13 22:58:33 +0900

    first commit

---
+++ head.php
@@ -0,0 +1,6 @@
+  <head>
+    <meta charset="utf-8">
+    <title><?php echo $siteTitle; ?> | WEBUKATU MARKET</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+  </head>
